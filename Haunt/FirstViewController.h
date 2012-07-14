96b92b9c63ecf8ee35f064563dc31c46549d07c4
Aubrey Goodman <aubrey.goodman@gmail.com>
2012-07-14 18:42:18 -0400

    Initial Commit

---
+++ Haunt/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Haunt
+//
+//  Created by Aubrey Goodman on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Haunt/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Haunt
+//
+//  Created by Aubrey Goodman on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
